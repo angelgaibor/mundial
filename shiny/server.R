53ec7fa4b177f10d852eb60f99c6fcd2c1b2e503
desarrollador-endi1 <endi.dm.inec1@gmail.com>
2022-11-15 16:15:15 -0500

    AG: ponelo en producción lindo

--- shiny/server.R
+++ shiny/server.R
@@ -15,10 +15,33 @@ server <- function(input, output, session){
     })
     
    lol <- reactive(
-     equipos %>% 
-     mutate(resul_gru = ifelse(Siglas == input$a1, "Primero del grupo A", ""))
-   )
+     
+      equipos %>%
+         mutate(resul_gru = case_when(Equipo %in% c(input$a1, input$b1, input$c1, input$d1,
+                                                input$e1, input$f1, input$g1, input$h1) ~ "Primero",
+                                      Equipo %in% c(input$a2, input$b2, input$c2, input$d2,
+                                                input$e2, input$f2, input$g2, input$h2) ~ "Segundo",
+                                  T ~ "")) %>% 
+         filter(resul_gru != "") %>% 
+         select(Siglas, Equipo, Grupo, Resultado = resul_gru)
+   )  
    
+   output$ib_res_con <- renderInfoBox({
+      if(dim(lol())[1] == 16){
+         colorib = "green"
+         mensaje = "Puede descargar sus resultados"
+      }else{
+         colorib = "red"
+         mensaje = "Seleccione un equipo diferente en cada grupo"
+      }
+      infoBox(
+         HTML("Estado de tu predicción"), 
+         h6(mensaje), 
+         icon = icon("cloud-upload", lib = "glyphicon"),
+         color = colorib, 
+         fill = T
+      )
+   })
    
    output$descarga <- downloadHandler(
      filename = function() {
@@ -28,6 +51,20 @@ server <- function(input, output, session){
        openxlsx::write.xlsx(lol(), file)
      }
    )
+   
+   output$tiempo <- renderValueBox({
+      invalidateLater(1000, session)
+      t <- difftime(as.POSIXlt.character("2022-11-20 11:00:00 -05"), Sys.time(), units = "days")
+      t <- as.numeric(t)
+      dias <- floor(t)
+      horas <- floor((t - floor(t))*24)
+      minutos <- floor(((t - floor(t))*24 - horas)*60)
+      segundos <- floor(t*24*60*60 - (dias*24*60*60 + horas*60*60 + minutos*60))
+      minutos <- str_pad(minutos, 2, "left", "0")
+      segundos <- str_pad(segundos, 2, "left", "0")
+      mensaje <- paste0(dias, " dias ", horas, ":", minutos, ":", segundos)
+      valueBox(mensaje, "restantes para Qatar 2022", icon = icon("time", lib = "glyphicon"), color = "yellow")
+   })
 }
 
 
--- shiny/ui.R
+++ shiny/ui.R
@@ -19,13 +19,13 @@ options(shiny.maxRequestSize=30*1024^2,
 #
 
 
-ui <- dashboardPage(skin="purple",
+ui <- dashboardPage(skin="black",
                     
-                    dashboardHeader(title = "Consistencia base de campo ENDI - GDM", titleWidth = 480
+                    dashboardHeader(title = "Polla mundialista Adeptos", titleWidth = 480
                     ),
                     
                     # panel lateral
-                    dashboardSidebar(width = 360,
+                    dashboardSidebar(width = 240,
                                      sidebarMenu(menuItem("Informacion", tabName = "info", selected = T, 
                                                           icon = icon("sort-by-alphabet", lib="glyphicon")),
                                                  menuItem("Juega conmigo", tabName = "juega",
@@ -38,92 +38,117 @@ ui <- dashboardPage(skin="purple",
                     dashboardBody(
                       tabItems(
                         tabItem("info", 
-                                fluidRow(infoBox(title = "Fecha de corte", value = 1,icon = icon("calendar")),
-                                         infoBoxOutput("ib_equ_com"))),
-                        tabItem("juega", h1("Sexo y fecha"),
-                                textInput("nombre", "Ingresa tu nombre", value = "..."),
+                                fluidRow(column(width = 12, valueBoxOutput("tiempo", width = 12)),
+                                         column(width = 6,
+                                                box(title = "Mundial Qatar 2022", status = "danger", solidHeader = T, width = 12,
+                                                    "Esto copiamos de wikipedia: La Copa Mundial de Fútbol de la FIFA Catar 2022 (en árabe, كأس العالم لكرة
+                                                    القدم قطر 2022) será la XXII edición de la Copa Mundial de Fútbol masculino 
+                                                    organizada por la FIFA. Se desarrollará desde el 20 de noviembre al 18 de diciembre en 
+                                                    Catar, que consiguió los derechos de organización el 2 de diciembre de 2010.",
+                                                    br(), br(),
+                                                    "En Ecuador no va jugar el Kitu :("),
+                                                box(title = "Reglas del juego", status = "danger", solidHeader = T, width = 12,
+                                                    "- En la pestaña Juega conmigo, ingresa tu nombre y selecciona el primer y segundo equipo de cada grupo.",br(),
+                                                    "- Debes guardar tus resultados dando click en el botón Su resultado una vez que hayas seleccionado a tus equipos", br(),
+                                                    "- El archivo excel que se descarga se debe enviar a los correos javier.ns87@gmail.com o angleito2112@gmail.com.", br(),
+                                                    "- De forma simbólica para participar se debe depositar $1, así se repartirá lo reunido entre las tres personas que obtengan mayor puntaje.", br(),
+                                                    "- 50% para el primer lugar, 30% para el segundo lugar y 20% para el tercer lugar", br(),
+                                                    "- El criterio de desempate en el caso de darse se definirá por quien tenga mayor número de aciertos en la posición de los equipos."),
+                                                box(title = "Sistema de puntos", status = "danger", solidHeader = T, width = 12,
+                                                    "- Por cada equipo clasificado a octavos de final recibes 1 punto.", br(),
+                                                    "- Además si aciertas a la posición de clasificación de tu equipo recibes un punto extra.", br()
+                                                )
+                                         ),
+                                         column(width = 6, img(src = "logo.jpg", style = "width:600px"))
+                                )
+                        ),
+                        tabItem("juega", h1("Tu predicción"),
+                                textInput("nombre", "Ingresa tu nombre/apodo", value = "..."),
+#####
                                 fluidRow(
                                   box(title = "Grupo A", status = "primary", solidHeader = T, width = 3,
-                                             selectizeInput("a1", label = NULL, equipos$Siglas[equipos$Grupo=="A"], 
+                                             selectizeInput("a1", label = NULL, equipos$Equipo[equipos$Grupo=="A"], 
                                                             multiple = F, 
                                                             options = list(placeholder = "Primero...", 
                                                                            onInitialize = I('function() { this.setValue("");}'))),
-                                             selectizeInput("a2", label = NULL, equipos$Siglas[equipos$Grupo=="A"], 
+                                             selectizeInput("a2", label = NULL, equipos$Equipo[equipos$Grupo=="A"], 
                                                             multiple = F, 
                                                             options = list(placeholder = "Segundo...", 
                                                                            onInitialize = I('function() { this.setValue("");}')))
                                       ),
                                   box(title = "Grupo B", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("b1", label = NULL, equipos$Siglas[equipos$Grupo=="B"], 
+                                      selectizeInput("b1", label = NULL, equipos$Equipo[equipos$Grupo=="B"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("b2", label = NULL, equipos$Siglas[equipos$Grupo=="B"], 
+                                      selectizeInput("b2", label = NULL, equipos$Equipo[equipos$Grupo=="B"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo C", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("c1", label = NULL, equipos$Siglas[equipos$Grupo=="C"], 
+                                      selectizeInput("c1", label = NULL, equipos$Equipo[equipos$Grupo=="C"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("c2", label = NULL, equipos$Siglas[equipos$Grupo=="C"], 
+                                      selectizeInput("c2", label = NULL, equipos$Equipo[equipos$Grupo=="C"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo D", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("d1", label = NULL, equipos$Siglas[equipos$Grupo=="D"], 
+                                      selectizeInput("d1", label = NULL, equipos$Equipo[equipos$Grupo=="D"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("d2", label = NULL, equipos$Siglas[equipos$Grupo=="D"], 
+                                      selectizeInput("d2", label = NULL, equipos$Equipo[equipos$Grupo=="D"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo E", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("e1", label = NULL, equipos$Siglas[equipos$Grupo=="E"], 
+                                      selectizeInput("e1", label = NULL, equipos$Equipo[equipos$Grupo=="E"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("e2", label = NULL, equipos$Siglas[equipos$Grupo=="E"], 
+                                      selectizeInput("e2", label = NULL, equipos$Equipo[equipos$Grupo=="E"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo F", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("f1", label = NULL, equipos$Siglas[equipos$Grupo=="F"], 
+                                      selectizeInput("f1", label = NULL, equipos$Equipo[equipos$Grupo=="F"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("f2", label = NULL, equipos$Siglas[equipos$Grupo=="F"], 
+                                      selectizeInput("f2", label = NULL, equipos$Equipo[equipos$Grupo=="F"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo G", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("g1", label = NULL, equipos$Siglas[equipos$Grupo=="G"], 
+                                      selectizeInput("g1", label = NULL, equipos$Equipo[equipos$Grupo=="G"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("g2", label = NULL, equipos$Siglas[equipos$Grupo=="G"], 
+                                      selectizeInput("g2", label = NULL, equipos$Equipo[equipos$Grupo=="G"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
                                   ),
                                   box(title = "Grupo H", status = "primary", solidHeader = T, width = 3,
-                                      selectizeInput("h1", label = NULL, equipos$Siglas[equipos$Grupo=="H"], 
+                                      selectizeInput("h1", label = NULL, equipos$Equipo[equipos$Grupo=="H"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Primero...", 
                                                                     onInitialize = I('function() { this.setValue("");}'))),
-                                      selectizeInput("h2", label = NULL, equipos$Siglas[equipos$Grupo=="H"], 
+                                      selectizeInput("h2", label = NULL, equipos$Equipo[equipos$Grupo=="H"], 
                                                      multiple = F, 
                                                      options = list(placeholder = "Segundo...", 
                                                                     onInitialize = I('function() { this.setValue("");}')))
-                                  )
+                                  ),
+                                  infoBoxOutput("ib_res_con")
                                 )
+#####
                                 ,
                                 downloadButton("descarga", label = "Su resultado",
                                                class = "btn-block")
