f504010117abb2082f85670a13d0dca5289b8c4b
desarrollador-endi1 <endi.dm.inec1@gmail.com>
2022-11-17 11:19:29 -0500

    AG: se generó la sintaxis que genera la tabla de posiciones, la misma está en global

--- shiny/R/global.R
+++ shiny/R/global.R
@@ -10,3 +10,43 @@ gs4_deauth()
 li_ligas <- "https://docs.google.com/spreadsheets/d/1G9vbPS4sYwRbAcs-325NpIFgr9VTnffCwZYlEyucWig/"
 lista_ligas <- read_sheet(li_ligas)
 
+partidos <- read.xlsx("shiny/data/partidos.xlsx")
+
+partidos_jugados <- partidos %>% 
+  filter(!is.na(GA))
+
+equipoa <- partidos_jugados %>% 
+  rename(Equipo = EquipoA) %>% 
+  mutate(J = 1,
+         G = ifelse(GA > GB, 1, 0),
+         P = ifelse(GA < GB, 1, 0),
+         E = ifelse(GA == GB, 1, 0),
+         GF = GA,
+         GC = GB) %>% 
+  select(Equipo, J, G, P, E, GF, GC)
+
+equipob <- partidos_jugados %>% 
+  rename(Equipo = EquipoB) %>% 
+  mutate(J = 1,
+         G = ifelse(GB > GA, 1, 0),
+         P = ifelse(GB < GA, 1, 0),
+         E = ifelse(GB == GA, 1, 0),
+         GF = GB,
+         GC = GA) %>% 
+  select(Equipo, J, G, P, E, GF, GC)
+
+equipoab <- rbind(equipoa, equipob) %>% 
+  group_by(Equipo) %>% 
+  summarise_all(sum)
+
+posiciones <- partidos %>% 
+  group_by(Grupo, Equipo = EquipoA) %>% 
+  summarise() %>% 
+  left_join(equipoab, by = "Equipo") %>% 
+  replace(is.na(.), 0) %>% 
+  mutate(DG = GF - GC,
+         Pts = G*3 + E) %>% 
+  arrange(Grupo, desc(Pts), desc(DG), desc(G)) %>% 
+  cbind(Pos = rep(c(1,2,3,4), 8)) %>% 
+  select(Grupo, Pos, Equipo, Pts, J, G, P, E, GF, GC, DG)
+  
