2b682e6dc03d2eab5c796aee6c0cfb8e247f194e
desarrollador-endi <endi.dm.inec@gmail.com>
2022-11-29 14:16:07 -0500

    JN: ocultar boton octavos

--- shiny/ui.R
+++ shiny/ui.R
@@ -433,8 +433,9 @@ ui <- dashboardPage(skin="black",
                                                 box(width = 12, tableOutput("tabla_cuartos")),
                                                 textInput("codigo_octavos", label = "Ingresa el código de participación", value = "",
                                                           width = "168px", placeholder = "Ejemplo: 1K1u5M"),
-                                                infoBoxOutput("ib_cod_oct", width = 12),
-                                                actionButton("pro_oct", "Envía tu pronóstico")
+                                                infoBoxOutput("ib_cod_oct", width = 12)
+                                                #,
+                                                #actionButton("pro_oct", "Envía tu pronóstico")
                                                 ),
                                          column(width = 1)
                                          )
